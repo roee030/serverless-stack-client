38fedebd218b62bff58563677db5d5e34689b75b
roee030 <roee.angel93@gmail.com>
2020-11-17 13:14:47 +0200

    add loader button for login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,50 +1,66 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./Login.css";
-import { Auth } from "aws-amplify";
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './Login.css';
+import { useAppContext } from '../libs/contextLib';
+import { Auth } from 'aws-amplify';
+import { useHistory } from 'react-router-dom';
+
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const history = useHistory();
+	const [isLoading, setIsLoading] = useState(false);
+	const { userHasAuthenticated } = useAppContext();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
+	function validateForm() {
+		return email.length > 0 && password.length > 0;
+	}
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    try {
-      await Auth.signIn(email, password);
-      alert("Logged in");
-    } catch (e) {
-      alert(e.message);
-    }
-  }
+	async function handleSubmit(event) {
+		event.preventDefault();
 
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
-    </div>
-  );
+		setIsLoading(true);
+
+		try {
+			await Auth.signIn(email, password);
+			userHasAuthenticated(true);
+			history.push('/');
+		} catch (e) {
+			alert(e.message);
+			setIsLoading(false);
+		}
+	}
+
+	return (
+		<div className="Login">
+			<Form onSubmit={handleSubmit}>
+				<Form.Group size="lg" controlId="email">
+					<Form.Label>Email</Form.Label>
+					<Form.Control
+						autoFocus
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</Form.Group>
+				<Form.Group size="lg" controlId="password">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</Form.Group>
+				<Button
+					block
+					size="lg"
+					type="submit"
+					disabled={!validateForm()}
+				>
+					Login
+				</Button>
+			</Form>
+		</div>
+	);
 }
